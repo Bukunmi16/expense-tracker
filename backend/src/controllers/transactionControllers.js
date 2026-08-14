bfbc8c6306b122c27e0bf4af3e779899feda3ec0
Bukunmi16 <bukunmiadigun14@gmail.com>
2026-08-14 20:37:56 +0100

    feat: implement CRUD operations for transactions in controllers and routes

--- backend/src/controllers/transactionControllers.js
+++ backend/src/controllers/transactionControllers.js
@@ -8,6 +8,14 @@ export const getAllTransactions = async (req, res) => {
         res.status(400).json({message: 'Error fetching all transactions', error})
     }
 }
+export const searchTransaction = async (req, res) => {
+    try {
+        const transaction = await Transaction.findById(req.params.id);
+        res.status(200).json(transaction ? transaction : {message : 'Transaction Not Found'})
+    } catch (error) {
+        res.status(400).json({message: 'Error searching for transaction', error})
+    }
+}
 
 export const createTransaction = async (req, res) => {
     try {
@@ -19,5 +27,32 @@ export const createTransaction = async (req, res) => {
     } catch (error) {
         console.log(error);
         res.status(400).json({message: 'Error recording transaction', error});
+    }
+}
+
+export const updateTransaction = async (req, res) => {
+    try {
+        const {description, amount, type, category} = req.body
+        const updatedTransaction = await Transaction.findByIdAndUpdate(req.param.id, {description, amount, type, category}, {new: true})
+        
+        if(!updatedTransaction) return res.status(404).json({message : 'Error Updating Transaction'})
+        
+        res.status(200).json(updateTransaction, {message: 'Transaction Updated Successfully'})
+        
+    } catch (error) {
+        
+    }
+}
+
+export const deleteTransaction = async (req, res) => {
+    try {
+        const deletedTransaction = await Transaction.findByIdAndDelete(req.param.id)
+        
+        if(!deletedTransaction) return res.status(404).json({message : 'Error Deleting Transaction'})
+        
+        res.status(200).json(deletedTransaction, {message : "Transaction Deleted Successfully"})
+        
+    } catch (error) {
+        
     }
 }
--- backend/src/routes/transactionRoutes.js
+++ backend/src/routes/transactionRoutes.js
@@ -1,12 +1,12 @@
 import express from 'express'
-import { getAllTransactions, createTransaction } from '../controllers/transactionControllers.js'
+import { getAllTransactions, createTransaction, searchTransaction, updateTransaction, deleteTransaction } from '../controllers/transactionControllers.js'
 
 const router = express.Router()
 
 router.get("/", getAllTransactions)
-// router.get("/:id", getOneTransaction)
 router.post("/", createTransaction)
-// router.put("/:id", updateTransaction)
-// router.get("/:id", deleteTransaction)
+router.get("/:id", searchTransaction)
+router.put("/:id", updateTransaction)
+router.delete("/:id", deleteTransaction)
 
 export default router
