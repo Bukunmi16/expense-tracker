27353c2e4318407e09c3f87f983f8e8f1bb6cf22
Bukunmi16 <bukunmiadigun14@gmail.com>
2026-08-19 13:05:25 +0100

    Test 2.1

--- frontend/src/components/GuestRoute.jsx
+++ frontend/src/components/GuestRoute.jsx
@@ -1,29 +1,17 @@
-// import { Navigate, Outlet } from "react-router";
-// import { useAuth } from "../context/AuthContext";
-
-// const GuestRoute = () => {
-//   const { user, loading } = useAuth();
-
-//   if (loading) {
-//     return <div className="flex min-h-screen items-center justify-center ">
-//         <p className="font-bold text-3xl text-center text-white">₦</p>
-// </div>
-//   }
-
-//   if (user) {
-//     return <Navigate to="/dashboard" replace />;
-//   }
-
-//   return <Outlet />;
-// };
-import { Outlet } from "react-router";
+import { Navigate, Outlet } from "react-router";
 import { useAuth } from "../context/AuthContext";
 
 const GuestRoute = () => {
-  const { loading } = useAuth();
+  const { user, loading } = useAuth();
 
   if (loading) {
-    return <div>Loading...</div>;
+    return <div className="flex min-h-screen items-center justify-center ">
+        <p className="font-bold text-3xl text-center text-white">₦</p>
+</div>
+  }
+
+  if (user) {
+    return <>USER EXISTS</>
   }
 
   return <Outlet />;
