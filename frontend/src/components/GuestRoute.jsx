e97cb4493e1994ca9739ca7caa16c9b76c64706b
Bukunmi16 <bukunmiadigun14@gmail.com>
2026-08-19 12:50:44 +0100

    test

--- frontend/src/components/GuestRoute.jsx
+++ frontend/src/components/GuestRoute.jsx
@@ -1,20 +1,27 @@
+// import { Navigate, Outlet } from "react-router";
+// import { useAuth } from "../context/AuthContext";
+
+// const GuestRoute = () => {
+//   const { user, loading } = useAuth();
+
+//   if (loading) {
+//     return <div className="flex min-h-screen items-center justify-center ">
+//         <p className="font-bold text-3xl text-center text-white">₦</p>
+// </div>
+//   }
+
+//   if (user) {
+//     return <Navigate to="/dashboard" replace />;
+//   }
+
+//   return <Outlet />;
+// };
+
+// export default GuestRoute;
 import { Navigate, Outlet } from "react-router";
-import { useAuth } from "../context/AuthContext";
 
 const GuestRoute = () => {
-  const { user, loading } = useAuth();
-
-  if (loading) {
-    return <div className="flex min-h-screen items-center justify-center ">
-        <p className="font-bold text-3xl text-center text-white">₦</p>
-</div>
-  }
-
-  if (user) {
-    return <Navigate to="/dashboard" replace />;
-  }
-
-  return <Outlet />;
+  return <div style={{ color: "black" }}>GuestRoute is working</div>;
 };
 
 export default GuestRoute;
